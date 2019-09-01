4dfea1103081853bf14fbe431b794554d2585f85
黄嘉恩 <kittyshiki@vip.qq.com>
2019-09-01 19:51:34 +0800

    搭建项目

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Header/>
+    <nuxt />
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*{
+  margin :0;
+  padding:0;
+}
+
+ul,li,ol{
+  list-style: none;
+}
+
+i,em{
+  font-style: normal;
+}
+
+a{
+color: inherit;
+text-decoration: none;
+}
+
+a:hover{
+
+}
+</style>
---
+++ pages/user/text.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="tab">
+      <span
+        v-for="(item,index) in ['我我','她她','他他']"
+        :key="index"
+        :class="{active:current===index}"
+        @click="handleclick(index)"
+      >{{ item }}</span>
+    </div>
+
+    <div v-show="current===0">
+      <ul>
+        <li>我我我</li>
+        <li>我我我</li>
+        <li>我我我</li>
+      </ul>
+    </div>
+
+    <div v-show="current===1">
+      <ul>
+        <li>她她她</li>
+        <li>她她她</li>
+        <li>她她她</li>
+      </ul>
+    </div>
+
+    <div v-show="current===2">
+      <ul>
+        <li>他他他</li>
+        <li>他他他</li>
+        <li>他他他</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      current: 0
+    }
+  },
+  methods: {
+    handleclick (index) {
+      this.current = index
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.tab {
+  cursor: pointer;
+  span{
+      padding-right: 10px
+  }
+}
+.active {
+  color: red;
+}
+</style>
